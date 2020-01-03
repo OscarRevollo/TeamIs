f624c722ade8cb83e8745858baa055713c077cd0
cidarparra72 <cid234.cp@gmail.com>
2020-01-03 10:02:43 -0400

    informaciones 2.1

---
+++ app/src/main/java/t2company/com/uy/teamis/CrearEvento.java
@@ -0,0 +1,14 @@
+package t2company.com.uy.teamis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_evento);
+    }
+}
--- app/src/main/java/t2company/com/uy/teamis/EventosActivity.java
+++ app/src/main/java/t2company/com/uy/teamis/EventosActivity.java
@@ -1,14 +1,39 @@
 package t2company.com.uy.teamis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class EventosActivity extends AppCompatActivity {
 
+    CardView crearEvento;
+    CardView eventos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eventos);
+        initViews();
+    }
+    private void initViews() {
+        crearEvento = findViewById(R.id.crear_evento);
+        eventos= findViewById(R.id.Eventos);
+
+
+        crearEvento.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext() ,CrearEvento.class));
+            }
+        });
+        eventos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Evento.class));
+            }
+        });
+
     }
 }
